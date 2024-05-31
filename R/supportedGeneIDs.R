b7717c5a573ed952aaa38fca228293a7f13ff0a8
Federico Marini <marinif@uni-mainz.de>
2024-05-31 16:44:11 +0200

    one full run of styler::style_pkg() on the full codebase

--- R/supportedGeneIDs.R
+++ R/supportedGeneIDs.R
@@ -1,27 +1,29 @@
-supportedGeneIDs=function(){
-	base=unfactor(txdbmaker::supportedUCSCtables())
+supportedGeneIDs <- function() {
+  base <- unfactor(txdbmaker::supportedUCSCtables())
 
-	#Current version
-	txname2gene_mapinfo=txdbmaker:::.UCSC_TXNAME2GENEID_MAPDEFS
-	base$GeneID=""
+  # Current version
+  txname2gene_mapinfo <- txdbmaker:::.UCSC_TXNAME2GENEID_MAPDEFS
+  base$GeneID <- ""
 
-	#Old version
-	#tmp=unlist(lapply(txname2gene_mapinfo[base$db],function(u){u[[2]]}))
-	#base$GeneID[match(names(tmp),base$db)]=as.character(tmp)
+  # Old version
+  # tmp=unlist(lapply(txname2gene_mapinfo[base$db],function(u){u[[2]]}))
+  # base$GeneID[match(names(tmp),base$db)]=as.character(tmp)
 
-	#NEW Ant 28/09/16
-	tmp=unlist(lapply(txname2gene_mapinfo[base$tablename],function(u){u[[2]]}))
-	base$GeneID[match(names(tmp),base$tablename)]=as.character(tmp)
+  # NEW Ant 28/09/16
+  tmp <- unlist(lapply(txname2gene_mapinfo[base$tablename], function(u) {
+    u[[2]]
+  }))
+  base$GeneID[match(names(tmp), base$tablename)] <- as.character(tmp)
 
-	#Add in gene symbol
-	base[nrow(base)+1,]=c("geneSymbol",'refGene','refFlat',"Gene Symbol")
-	base$AvailableGenomes=""
-	repo=grep(".*\\..*\\.LENGTH",as.data.frame(data(package="geneLenDataBase")$results,stringsAsFactors=FALSE)$Item,ignore.case=TRUE,value=TRUE)
-	repo=matrix(unlist(strsplit(repo,"\\.")),ncol=3,byrow=TRUE)
-	valid_genomes=sapply(split(repo[,1],repo[,2]),paste,collapse=",")
-	valid_genomes=valid_genomes[which(names(valid_genomes)%in%base$tablename)]
-	base$AvailableGenomes[match(names(valid_genomes),base$tablename)]=as.character(valid_genomes)
-  
-  avail = base[base$tablename == "knownGene" | base$tablename == "vegaGene" | base$tablename == "geneSymbol" | base$tablename == "ensGene",]
+  # Add in gene symbol
+  base[nrow(base) + 1, ] <- c("geneSymbol", "refGene", "refFlat", "Gene Symbol")
+  base$AvailableGenomes <- ""
+  repo <- grep(".*\\..*\\.LENGTH", as.data.frame(data(package = "geneLenDataBase")$results, stringsAsFactors = FALSE)$Item, ignore.case = TRUE, value = TRUE)
+  repo <- matrix(unlist(strsplit(repo, "\\.")), ncol = 3, byrow = TRUE)
+  valid_genomes <- sapply(split(repo[, 1], repo[, 2]), paste, collapse = ",")
+  valid_genomes <- valid_genomes[which(names(valid_genomes) %in% base$tablename)]
+  base$AvailableGenomes[match(names(valid_genomes), base$tablename)] <- as.character(valid_genomes)
+
+  avail <- base[base$tablename == "knownGene" | base$tablename == "vegaGene" | base$tablename == "geneSymbol" | base$tablename == "ensGene", ]
   avail
 }
--- R/unfactor.R
+++ R/unfactor.R
@@ -1,26 +1,26 @@
-unfactor=function(var){
-	if (is.factor(var)){
-		tmp=names(var)
-		tmpopt=getOption("warn")
-		options(warn=-1)
-		out=as.numeric(levels(var))[as.integer(var)]
-		options(warn=tmpopt)
-		if(any(is.na(out)) & any(is.na(out)!=is.na(var))){
-			out=as.character(levels(var))[as.integer(var)]
-		}
-		names(out)=tmp
-	}else if(is.data.frame(var)){
-		#Have to use a loop, since calling apply will return a matrix
-		out=var
-		for(i in 1:dim(var)[2]){
-			out[,i]=unfactor(var[,i])
-		}
-	}else if(is.list(var)){
-		#Mmmmm, recursion
-		out=lapply(var,unfactor)
-	}else{
-		#The default option
-		out=var
-	}
-	return(out)
+unfactor <- function(var) {
+  if (is.factor(var)) {
+    tmp <- names(var)
+    tmpopt <- getOption("warn")
+    options(warn = -1)
+    out <- as.numeric(levels(var))[as.integer(var)]
+    options(warn = tmpopt)
+    if (any(is.na(out)) & any(is.na(out) != is.na(var))) {
+      out <- as.character(levels(var))[as.integer(var)]
+    }
+    names(out) <- tmp
+  } else if (is.data.frame(var)) {
+    # Have to use a loop, since calling apply will return a matrix
+    out <- var
+    for (i in 1:dim(var)[2]) {
+      out[, i] <- unfactor(var[, i])
+    }
+  } else if (is.list(var)) {
+    # Mmmmm, recursion
+    out <- lapply(var, unfactor)
+  } else {
+    # The default option
+    out <- var
+  }
+  return(out)
 }
